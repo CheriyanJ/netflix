e8ae1bf140b2a526ab87983d508e00645ead954f
jamescheriyan <63226335+jamescheriyan@users.noreply.github.com>
2022-12-31 07:11:43 +0530

    na

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import "./app.scss";
+import Home from "./pages/home/Home";
+import Register from "./pages/register/Register";
+import Login from "./pages/login/Login";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="app">
+        <Routes>
+          <Route path="/" element={<Register/>} />
+          <Route path="/main" element={<Home/>} />
+          <Route path="/signup" element={<Login/>} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
